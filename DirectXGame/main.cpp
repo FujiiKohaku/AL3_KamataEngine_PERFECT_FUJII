0b019fcb8eed223a7debb922c2e518013435f409
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-04-18 09:55:09 +0900

    aaa

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -37,11 +37,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// 描画終了
 		dxComon->PostDraw();
 
-		// ゲームシーンの解放
-		delete gameScene;
 		// nullptrの代入
 		gameScene = nullptr;
 	}
+	// ゲームシーンの解放
+	delete gameScene;
 	// エンジンの終了処理
 	KamataEngine::Finalize();
 
