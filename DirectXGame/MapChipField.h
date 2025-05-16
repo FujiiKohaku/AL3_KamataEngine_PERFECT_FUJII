f972e30c916ca90bcc5f937d1a6bb9a9980a428c
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-16 11:18:28 +0900

    a

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "KamataEngine.h"
 class MapChipField {
 public:
-
-
 private:
+	// ブロックの個数
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
+	// 1ブロックのサイズ
+	static inline const uint32_t kNumBlockVertical = 20;
+	static inline const uint32_t kNumBlockHorizontal = 100;
 };
