453cd97b0ce98f80385c6d9f7f7aba663bb09d22
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-05-16 11:32:19 +0900

    a

--- DirectXGame/MapChipField.cpp
+++ DirectXGame/MapChipField.cpp
@@ -1,5 +1,7 @@
 #include "MapChipField.h"
+#include <fstream>
 #include <map>
+#include <sstream>
 namespace {
 std::map<std::string, MapChipType> mapChipTable = {
     {"0", MapChipType::kBlank},
@@ -13,5 +15,40 @@ void MapChipField::ResetMapChipData() {
 	mapChipDate_.data.resize(kNumBlockVertical);
 	for (std::vector<MapChipType>& mapChipDateLine : mapChipDate_.data) {
 		mapChipDateLine.resize(kNumBlockHorizontal);
+	}
+}
+
+void MapChipField::LoadMapChipCsv(const std::string& filepath) {
+
+	// マップチップデータをリセット
+	ResetMapChipData();
+	// ファイルを開く
+	std::ifstream file;
+	file.open(filepath);
+	assert(file.is_open());
+
+	// マップチップCSV
+	std::stringstream mapChipCsv;
+	// ファイルの内容を文字列ストリームにコピー
+	mapChipCsv << file.rdbuf();
+	// ファイルを閉じる
+	file.close();
+	// CSVからマップチップデータを読み込む
+	for (uint32_t i = 0; i < kNumBlockVertical; ++i) {
+		std::string line;
+		std::getline(mapChipCsv, line);
+
+		// １行文の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+
+			std::string word;
+			std::getline(line_stream, word, ',');
+
+			if (mapChipTable.contains(word)) {
+				mapChipDate_.data[i][j] = mapChipTable[word];
+			}
+		}
 	}
 }
