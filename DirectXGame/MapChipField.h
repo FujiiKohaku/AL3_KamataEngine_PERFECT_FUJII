936827da0e5ceb6e6bd95acc6f39302aa91763b2
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-06-04 13:07:01 +0900

    AL3_02_07 p27までOK

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -24,6 +24,15 @@ public:
 
 	KamataEngine::Vector3 GetMapChipPositionByIndex(uint32_t xIndex, uint32_t yIndex);
 
+	//2Dマップのインデックス位置（列・行）を表すためのデータ型AL3_02_07page22
+	struct IndexSet {
+		uint32_t xIndex;
+		uint32_t yIndex;
+	};
+
+	//IndexSet GetMapChipIndexSetByPosition(const Vector3& position);
+
+
 private:
 	// ブロックの個数
 	static inline const float kBlockWidth = 1.0f;
